983f509dc2598fb47616ba16fdc5319a07338df9
Gee Chai <fc00004@gmail.com>
2024-02-20 21:32:23 +0900

    feat: Navbar added to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import Image from "next/image";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div>
+      <div className={styles.logoContainer}>
         <Image
           src="/next.svg" // Assuming you have a Next.js logo SVG in your public directory
           alt="Next.js Logo"
@@ -15,7 +16,16 @@ const Header: React.FC = () => {
           height={64} // Adjust to the size of your actual logo
         />
       </div>
-      {/* Add navigation items or other content here */}
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link href="/about">About</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href="/contact-page">Contact Page</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
