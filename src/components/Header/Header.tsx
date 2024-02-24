749b5257c183fd18cec541bc4cb7584b91a0c489
Gee Chai <fc00004@gmail.com>
2024-02-24 20:50:30 +0900

    feat: click on logo sends to homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,13 +9,15 @@ const Header: React.FC = () => {
   return (
     <header className={`${poppins.className} ${styles.header}`}>
       <div className={styles.logoContainer}>
-        <Image
-          src="/next.svg" // Assuming you have a Next.js logo SVG in your public directory
-          alt="Next.js Logo"
-          className={styles.logo}
-          width={128} // Adjust to the size of your actual logo
-          height={64} // Adjust to the size of your actual logo
-        />
+        <Link href="/">
+          <Image
+            src="/next.svg" // Assuming you have a Next.js logo SVG in your public directory
+            alt="Next.js Logo"
+            className={styles.logo}
+            width={128} // Adjust to the size of your actual logo
+            height={64} // Adjust to the size of your actual logo
+          />
+        </Link>
       </div>
       <nav className={styles.nav}>
         <ul className={styles.navList}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Bake</h1>
+      <h1 className={styles.heading}>Home Page</h1>
     </div>
   );
 }
