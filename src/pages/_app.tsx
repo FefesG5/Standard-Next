5fe36eb052087107b38eec5f60d5190bb1f65022
Gee Chai <fc00004@gmail.com>
2024-03-17 13:44:05 +0900

    feat: ThemeContext component setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import RootLayout from "@/app/RootLayout";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
+    <ThemeProvider>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </ThemeProvider>
   );
 }
 
