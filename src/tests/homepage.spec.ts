feb8bdec4e9061f23af66d324a66fc55f2a0ab7e
Gee Chai <fc00004@gmail.com>
2024-04-04 13:25:55 +0900

    test: Home Page document title and h1 heading checks

--- src/tests/homepage.spec.ts
+++ src/tests/homepage.spec.ts
@@ -1,11 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test("home page has correct heading", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+test.describe("Home Page test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000");
+  });
 
-  await expect(page).toHaveTitle(/Create Next App/i);
+  test("Home Page document title is Create Next App", async ({ page }) => {
+    await expect(page).toHaveTitle(/Create Next App/i);
+  });
 
-  const heading = page.locator("h1", { hasText: "Home Page" });
-
-  await expect(heading).toBeVisible();
+  test("Home Page has correct h1 heading", async ({ page }) => {
+    const heading = page.locator("h1", { hasText: "Home Page" });
+    await expect(heading).toBeVisible();
+  });
 });
