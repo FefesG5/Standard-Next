760f1202ecf8ac50cf0cef2bdc2655f88e5c0d9b
Gee Chai <fc00004@gmail.com>
2024-05-06 14:14:24 +0900

    fix: signOutButton imported into access page

--- src/pages/access.tsx
+++ src/pages/access.tsx
@@ -1,10 +1,24 @@
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { app } from "../../firebase.config";
 import SignIn from "@/components/SignIn/SignIn";
+import SignOutButton from "@/components/SignOutButton/SignOutButton";
 
 const Access: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
   return (
     <>
       <h1>Access Component</h1>
-      <SignIn />
+      {user ? <SignOutButton /> : <SignIn />}
     </>
   );
 };
