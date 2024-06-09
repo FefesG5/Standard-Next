8d48a4f52d628567daf9128689eac7598c82843c
Gee Chai <fc00004@gmail.com>
2024-06-09 11:37:49 +0900

    feat: loading status added, Spinner to display during loading

--- src/pages/access.tsx
+++ src/pages/access.tsx
@@ -1,20 +1,28 @@
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { app } from "../../firebase.config";
+import Spinner from "@/components/Spinner/Spinner";
 import SignIn from "@/components/SignIn/SignIn";
 import UserSection from "@/components/UserSection/UserSection";
 
 const Access: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const auth = getAuth(app);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, [auth]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return <>{user ? <UserSection user={user} /> : <SignIn />}</>;
 };
 
