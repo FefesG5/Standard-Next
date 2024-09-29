2e533dc4611e16964c2f487d9dc1821b40e60ef2
Gee Chai <fc00004@gmail.com>
2024-09-29 16:01:36 +0900

    refactor: modified SignOutButton to take a signOutUser property

--- src/components/SignOutButton/SignOutButton.tsx
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -1,31 +1,12 @@
-import { getAuth, signOut } from "firebase/auth";
-import { useState } from "react";
-import { app } from "../../../firebase.config";
-import styles from "./signoutbutton.module.css";
+interface SignOutButtonProps {
+  signOutUser: () => Promise<void>;
+}
 
-const SignOutButton = () => {
-  const [message, setMessage] = useState("");
-  const auth = getAuth(app);
-
-  const signOutUser = () => {
-    signOut(auth)
-      .then(() => {
-        console.log("Signed out successfully.");
-        setMessage("Signed out successfully.");
-      })
-      .catch((error) => {
-        console.error("Error signing out:", error);
-        setMessage("An error occurred while signing out.");
-      });
-  };
-
+const SignOutButton: React.FC<SignOutButtonProps> = ({ signOutUser }) => {
   return (
-    <div>
-      <button className={styles.button} onClick={signOutUser}>
-        Sign Out
-      </button>
-      {message && <p className={styles.message}>{message}</p>}
-    </div>
+    <button onClick={signOutUser} className="signOutButton">
+      Sign Out
+    </button>
   );
 };
 
