c5b03eaa298582bb9b26bf2f800b7db4911f8e4a
Gee Chai <fc00004@gmail.com>
2024-10-01 11:51:39 +0900

    refactor: integrated dashboardNav data into displaying dashboardNavigation Items and icons

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 import UserSection from "../UserSection/UserSection";
 import { User } from "firebase/auth";
+import { dashboardNav } from "@/config/dashBoardNav";
+
 import styles from "./Dashboard.module.css";
 
 type DashboardProps = {
@@ -19,26 +22,22 @@ const Dashboard = ({ signOutUser, user }: DashboardProps) => {
       <div className={styles.dashboardContent}>
         <nav className={styles.dashboardNav}>
           <ul className={styles.navList}>
-            <li className={styles.navItem}>
-              <Link href="/dashboard/home" className={styles.navLink}>
-                Home
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link href="/dashboard/profile" className={styles.navLink}>
-                Profile
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link href="/dashboard/settings" className={styles.navLink}>
-                Settings
-              </Link>
-            </li>
-            <li className={styles.navItem}>
-              <Link href="/dashboard/help" className={styles.navLink}>
-                Help
-              </Link>
-            </li>
+            {dashboardNav.map((item) => (
+              <li className={styles.navItem} key={item.href}>
+                <Link href={item.href} className={styles.navLink}>
+                  {item.icon && (
+                    <Image
+                      src={item.icon}
+                      alt={`${item.label} icon`}
+                      width={50}
+                      height={50}
+                      className={styles.navIcon}
+                    />
+                  )}
+                  {item.label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
         <main className={styles.mainContent}>
