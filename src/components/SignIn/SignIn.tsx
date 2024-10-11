84b6539dde44c45b8e1f24f73b240fac9616db4b
Gee Chai <fc00004@gmail.com>
2024-10-11 20:26:14 +0900

    refactor: integrated tailwind into SignIn component

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -7,7 +7,6 @@ import {
 } from "firebase/auth";
 import { useState } from "react";
 import { app } from "../../../firebase.config";
-import styles from "./SignIn.module.css";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -45,35 +44,45 @@ const SignIn = () => {
   };
 
   return (
-    <div className={styles.signInContainer}>
+    <div className="text-center p-8 bg-[var(--signin-container-bg-color)] border border-[var(--signin-input-border-color)] rounded-md shadow-md max-w-full my-8 mx-auto sm:max-w-xs md:max-w-md lg:max-w-lg">
       <input
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter your email"
-        className={styles.emailInputField}
+        className="w-full p-3 my-2 rounded-md text-base outline-none bg-[var(--signin-input-bg-color)] border border-[var(--signin-input-border-color)] focus:border-blue-600 focus:ring focus:ring-blue-200"
       />
-      <button onClick={sendEmailLink} className={styles.signInButton}>
+      <button
+        onClick={sendEmailLink}
+        className="w-full flex items-center justify-center p-2 my-2 bg-[var(--signin-btn-bg-color)] text-[var(--signin-text-color)] rounded-lg cursor-pointer font-bold text-base hover:bg-[var(--signin-btn-hover-bg-color)] transition-colors duration-200 ease-in-out"
+      >
         <Image
           src="/email-icon.png"
           alt="Email Sign-in Icon"
           width={40}
           height={40}
-          className={styles.emailIcon}
+          className="mr-2"
         />
-        <span className={styles.emailSignInText}>Sign in with Email link</span>
+        <span>Sign in with Email link</span>
       </button>
-      <button onClick={signInWithGoogle} className={styles.googleSignInButton}>
+      <button
+        onClick={signInWithGoogle}
+        className="w-full flex items-center justify-center p-3 my-2 bg-[var(--signin-btn-bg-color)] text-[var(--signin-text-color)] rounded-lg cursor-pointer font-bold text-base hover:bg-[var(--signin-btn-hover-bg-color)] transition-colors duration-200 ease-in-out"
+      >
         <Image
           src="/web-neutral-rd-na.svg"
           alt="Google Sign-in Logo"
           width={30}
           height={30}
-          className={styles.googleIcon}
+          className="mr-3"
         />
-        <span className={styles.googleSignInText}>Sign in with Google</span>
+        <span>Sign in with Google</span>
       </button>
-      {message && <p className={styles.signInMessage}>{message}</p>}
+      {message && (
+        <p className="mt-4 text-[var(--error-message-text-color)] text-sm font-bold">
+          {message}
+        </p>
+      )}
     </div>
   );
 };
