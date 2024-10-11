2ad64c375efd8fa147b3e0b34a41c307b7e11fff
Gee Chai <fc00004@gmail.com>
2024-10-11 20:17:08 +0900

    refactor: integrated tailwind into NavItem components

--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-import headerStyles from "../Header/Header.module.css";
-import sidebarStyles from "../Sidebar/Sidebar.module.css";
 
 interface NavItemProps {
   href: string;
@@ -15,17 +13,27 @@ const NavItem: React.FC<NavItemProps> = ({
   variant,
   closeSidebar,
 }) => {
+  // Define the Tailwind styles for each variant
   const getClassName = () => {
     switch (variant) {
       case "header":
-        return headerStyles.navItem;
+        return "px-3 py-1 mx-2 text-base hover:text-blue-500 transition duration-200 ease-in-out";
       case "sidebar":
-        return sidebarStyles.navItem;
+        return "block px-6 py-4 text-lg font-medium border-b transition-all duration-300 ease-in-out";
+      default:
+        return "";
     }
   };
 
   return (
-    <li className={getClassName()} onClick={closeSidebar}>
+    <li
+      className={getClassName()}
+      style={{
+        color: "var(--body-text-color)",
+        borderBottomColor: "var(--sidebar-bg-hover)",
+      }}
+      onClick={closeSidebar}
+    >
       <Link data-testid={`nav-${label.toLowerCase()}`} href={href}>
         {label}
       </Link>
