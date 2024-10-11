7b938431cdb7fadc1d97848a341c1425989bb200
Gee Chai <fc00004@gmail.com>
2024-10-11 20:53:44 +0900

    refactor: integrated tailwind into Spinner component

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,9 +1,12 @@
-import styles from "./Spinner.module.css";
-
 const Spinner = () => {
   return (
-    <div className={styles.spinnerWrapper}>
-      <div className={styles.spinner}></div>
+    <div className="fixed top-[20%] left-1/2 transform -translate-x-1/2 w-full h-0 flex justify-center items-center">
+      <div
+        className="w-10 h-10 border-4 border-solid rounded-full border-t-[var(--spinnner-border-left-color)] border-gray-300 animate-spin"
+        style={{
+          borderTopColor: "var(--spinnner-border-left-color)",
+        }}
+      ></div>
     </div>
   );
 };
