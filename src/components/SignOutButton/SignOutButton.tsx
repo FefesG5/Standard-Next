05da326bad99eda5bdfe0f6026a5d84f5f791662
Gee Chai <fc00004@gmail.com>
2024-10-11 20:31:36 +0900

    refactor: integrated tailwind into SignOutButton component

--- src/components/SignOutButton/SignOutButton.tsx
+++ src/components/SignOutButton/SignOutButton.tsx
@@ -4,7 +4,10 @@ interface SignOutButtonProps {
 
 const SignOutButton: React.FC<SignOutButtonProps> = ({ signOutUser }) => {
   return (
-    <button onClick={signOutUser} className="signOutButton">
+    <button
+      onClick={signOutUser}
+      className="bg-[var(--signin-btn-bg-color)] text-[var(--signin-text-color)] border-none py-2 px-4 md:py-3 md:px-6 text-sm md:text-base font-bold rounded-lg cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[var(--signin-btn-hover-bg-color)]"
+    >
       Sign Out
     </button>
   );
