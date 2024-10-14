768a2bdf883fd7d9d331701b70328e36c57f80ca
Gee Chai <fc00004@gmail.com>
2024-10-14 16:34:52 +0900

    feat: dashboard protected and requires user to log in

--- src/pages/access/dashboard/index.tsx
+++ src/pages/access/dashboard/index.tsx
@@ -1,7 +1,35 @@
-export default function Dashboard() {
+import DashboardLayout from "@/components/Dashboard/DashboardLayout";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { app } from "../../../../firebase.config";
+import Spinner from "@/components/Spinner/Spinner";
+
+export default function DashboardHome() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (!user) {
+    return <p>You must be logged in to access the dashboard.</p>;
+  }
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <DashboardLayout user={user} signOutUser={() => auth.signOut()}>
+      <h1>Dashboard Home</h1>
+      <p>Welcome to the dashboard! You can manage everything from here.</p>
+    </DashboardLayout>
   );
 }
